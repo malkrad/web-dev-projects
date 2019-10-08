5fd645ec27b26c5da8b87b90be026fe83229b0b8
Mo <m.alkrad@hotmail.com>
2019-10-08 16:48:32 +0200

    Tasks can be added.

--- tasklist/app.js
+++ tasklist/app.js
@@ -5,3 +5,29 @@ const clearBtn = document.querySelector('.clear-tasks');
 const filter = document.querySelector('#filter');
 const taskInput = document.querySelector('#task');
 
+loadEventListener();
+
+function loadEventListener() {
+    form.addEventListener('submit', addTask);
+}
+
+function addTask(e) {
+    if (taskInput.value === '') {
+        alert('Please enter a task first.')
+    } else {
+        const listItem = document.createElement('li');
+        listItem.className = 'collection-item';
+        listItem.appendChild(document.createTextNode(taskInput.value));
+
+        const deleteItem = document.createElement('a');
+        deleteItem.className = 'delelte-item secondary-content';
+        deleteItem.innerHTML = '<i class="fa fa-remove"></i>';
+
+        listItem.appendChild(deleteItem);
+
+        taskList.appendChild(listItem);
+
+        taskInput.value = '';
+    }
+    e.preventDefault();
+}
